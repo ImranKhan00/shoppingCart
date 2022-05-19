a2a4d5fe51d683fb75e219d9f64eb6510e02a707
Imran Khan <imran.khan@orgonize.com>
2022-05-19 15:35:03 +0500

    Get request Of methods edit,create,details done

--- cmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ cmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -14,21 +14,33 @@ namespace cmsShoppingCart.Areas.Admin.Controllers
 	public class PagesController : Controller
 	{
 		private readonly shoppingCartDbContext context;
-		public PagesController(shoppingCartDbContext	context)
+		public PagesController(shoppingCartDbContext context)
 		{
 			this.context = context;
 		}
 
-
+		//GET admin/pages
 		public async Task<ActionResult> Index()
 		{
-			IQueryable<Page> pages = from p in context.Pages 
-															 orderby p.Sorting 
+			IQueryable<Page> pages = from p in context.Pages
+															 orderby p.Sorting
 															 select p;
 
 			List<Page> pageList = await pages.ToListAsync();
 			return View(pageList);
 		}
+		//GET admin/pages/details/5
+		public async Task<ActionResult> Details(int id)
+		{
+			Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+			if (page == null)
+			{
+				return NotFound();
+			}
+			return View(page);
+		}
 
+		//GET admin/pages/create
+		public IActionResult Create() => View();
 	}
 }
