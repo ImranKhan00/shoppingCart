8ea844ea9cb2c983ae422f9b914a3b9b298e86ef
Imran Khan <imran.khan@orgonize.com>
2022-05-19 13:04:08 +0500

    firstCommit

---
+++ cmsShoppingCart/Infrastructure/shoppingCartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using cmsShoppingCart.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace cmsShoppingCart.Infrastructure
+{
+	public class shoppingCartDbContext:DbContext
+	{
+		public shoppingCartDbContext(DbContextOptions<shoppingCartDbContext> options):base(options)
+		{
+
+		}
+
+		public DbSet<Pages> Pages { get; set; }
+	}
+}
